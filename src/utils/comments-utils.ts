f4b6d365c881612578df8280f9a0ac0762a00640
SvetozarMiskovic <svetomiskovic98@gmail.com>
2024-10-11 00:31:58 +0200

    git cache cleared

--- src/utils/comments-utils.ts
+++ src/utils/comments-utils.ts
@@ -7,63 +7,75 @@ import type {
 } from '../types/comments-types';
 
 const newComment = async ({ author, comment, operation }: CommentPayload) => {
-  const response = await fetch('/api/comments', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application.json',
-    },
-    body: JSON.stringify({
-      author,
-      comment,
-      operation,
-    }),
-  });
+  const response = await fetch(
+    'https://comments-astrojs.vercel.app/api/comments',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application.json',
+      },
+      body: JSON.stringify({
+        author,
+        comment,
+        operation,
+      }),
+    }
+  );
 
   return response;
 };
 const editComment = async ({ id, author, comment, operation }: CommentEdit) => {
-  const response = await fetch('/api/comments', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application.json',
-    },
-    body: JSON.stringify({
-      id,
-      author,
-      comment,
-      operation,
-    }),
-  });
+  const response = await fetch(
+    'https://comments-astrojs.vercel.app/api/comments',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application.json',
+      },
+      body: JSON.stringify({
+        id,
+        author,
+        comment,
+        operation,
+      }),
+    }
+  );
 
   return response;
 };
 
 const deleteComment = async ({ id, operation }: CommentDelete) => {
-  const response = await fetch('/api/comments', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application.json',
-    },
-    body: JSON.stringify({
-      id,
-      operation,
-    }),
-  });
+  const response = await fetch(
+    'https://comments-astrojs.vercel.app/api/comments',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application.json',
+      },
+      body: JSON.stringify({
+        id,
+        operation,
+      }),
+    }
+  );
   return response;
 };
 
 const archiveComment = async ({ id, archived, operation }: CommentArchive) => {
-  const response = await fetch('/api/comments', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application.json',
-    },
-    body: JSON.stringify({
-      id,
-      archived,
-      operation,
-    }),
-  });
+  const response = await fetch(
+    'https://comments-astrojs.vercel.app/api/comments',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application.json',
+      },
+      body: JSON.stringify({
+        id,
+        archived,
+        operation,
+      }),
+    }
+  );
   return response;
 };
 
