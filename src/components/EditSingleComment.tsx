bdcae9dd9c564e628dc7308d421fa0bbe2a82b29
SvetozarMiskovic <svetomiskovic98@gmail.com>
2024-10-11 19:06:07 +0200

    fixed a bug for displaying edit single comment

--- src/components/EditSingleComment.tsx
+++ src/components/EditSingleComment.tsx
@@ -81,82 +81,81 @@ export default function EditSingleComment({ paramId }: { paramId: string }) {
     getCommentsHandle();
   }, []);
 
-  if (editAuthor)
-    return (
-      <form
-        onSubmit={() =>
-          handleEdit({
-            author: editAuthor,
-            id: Number(id),
-            comment: editComment,
-            operation: 'edit',
-          })
-        }
-        className="bg-emerald-800 rounded  max-w-md mx-auto"
-      >
-        <div className="flex flex-col mx-auto justify-start gap-4">
-          <label className="flex flex-col px-2 py-2 gap-2 font-bold text-xl text-white cursor-pointer">
-            Author
-            <input
-              onChange={(e) => setEditAuthor(e.target.value)}
-              required
-              value={editAuthor}
-              className="p-2 outline-none max-w-md rounded text-emerald-800"
-              type="text"
-              name="author"
-            />
-          </label>
-          <label className="flex flex-col px-2 py-2 gap-2 font-bold text-white cursor-pointer text-xl">
-            Your comment
-            <textarea
-              required
-              onChange={(e) => setEditComment(e.target.value)}
-              value={editComment}
-              className="p-2 leading-6 outline-none max-w-md rounded text-emerald-800 min-h-32 h-auto"
-              name="comment"
-            />
-          </label>
-          <label className="flex flex-col px-2 py-2 gap-2 font-bold text-white cursor-pointer text-xl">
-            Comment archived status
-            <input
-              disabled
-              className="p-2 outline-none max-w-md rounded border border-emerald-900 text-emerald-800"
-              type="text"
-              value={editArchived ? 'Archived' : 'Not archived'}
-            />
-          </label>
+  return (
+    <form
+      onSubmit={() =>
+        handleEdit({
+          author: editAuthor,
+          id: Number(id),
+          comment: editComment,
+          operation: 'edit',
+        })
+      }
+      className="bg-emerald-800 rounded  max-w-md mx-auto"
+    >
+      <div className="flex flex-col mx-auto justify-start gap-4">
+        <label className="flex flex-col px-2 py-2 gap-2 font-bold text-xl text-white cursor-pointer">
+          Author
+          <input
+            onChange={(e) => setEditAuthor(e.target.value)}
+            required
+            value={editAuthor}
+            className="p-2 outline-none max-w-md rounded text-emerald-800"
+            type="text"
+            name="author"
+          />
+        </label>
+        <label className="flex flex-col px-2 py-2 gap-2 font-bold text-white cursor-pointer text-xl">
+          Your comment
+          <textarea
+            required
+            onChange={(e) => setEditComment(e.target.value)}
+            value={editComment}
+            className="p-2 leading-6 outline-none max-w-md rounded text-emerald-800 min-h-32 h-auto"
+            name="comment"
+          />
+        </label>
+        <label className="flex flex-col px-2 py-2 gap-2 font-bold text-white cursor-pointer text-xl">
+          Comment archived status
+          <input
+            disabled
+            className="p-2 outline-none max-w-md rounded border border-emerald-900 text-emerald-800"
+            type="text"
+            value={editArchived ? 'Archived' : 'Not archived'}
+          />
+        </label>
 
-          <div className="w-full max-w-md px-2 py-2 relative flex gap-3 justify-center max-md:flex-col ">
-            <button
-              type="submit"
-              className="min-w-28 bg-green-600 p-2 rounded font-bold text-white hover:bg-green-400 cursor-pointer transition:ease-in duration-500"
-            >
-              Save
-            </button>
-            <button
-              type="button"
-              onClick={() =>
-                handleArchive({
-                  archived: !editArchived,
-                  id: Number(id),
-                  operation: 'archive',
-                })
-              }
-              className="min-w-28 bg-blue-500 p-2 rounded font-bold text-white hover:bg-blue-700 cursor-pointer transition:ease-in duration-500"
-            >
-              {editArchived ? 'Restore' : 'Archive'}
-            </button>
-            <button
-              onClick={() =>
-                handleDelete({ id: Number(id), operation: 'delete' })
-              }
-              type="button"
-              className="min-w-28 bg-red-800 p-2 rounded font-bold text-white hover:bg-red-600 cursor-pointer transition:ease-in duration-500"
-            >
-              Delete
-            </button>
-          </div>
+        <div className="w-full max-w-md px-2 py-2 relative flex gap-3 justify-center max-md:flex-col ">
+          <button
+            type="submit"
+            className="min-w-28 bg-green-600 p-2 rounded font-bold text-white hover:bg-green-400 cursor-pointer transition:ease-in duration-500"
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              handleArchive({
+                archived: !editArchived,
+                id: Number(id),
+                operation: 'archive',
+              })
+            }
+            className="min-w-28 bg-blue-500 p-2 rounded font-bold text-white hover:bg-blue-700 cursor-pointer transition:ease-in duration-500"
+          >
+            {editArchived ? 'Restore' : 'Archive'}
+          </button>
+          <button
+            onClick={() =>
+              handleDelete({ id: Number(id), operation: 'delete' })
+            }
+            type="button"
+            className="min-w-28 bg-red-800 p-2 rounded font-bold text-white hover:bg-red-600 cursor-pointer transition:ease-in duration-500"
+          >
+            Delete
+          </button>
         </div>
-      </form>
-    );
+      </div>
+    </form>
+  );
 }
